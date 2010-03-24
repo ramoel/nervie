42647e10f6fee681e153c4c62cfd103d386e030f
Ramoel <ramoel@ramoel-laptop.(none)>
2010-03-24 20:34:22 -0300

    arrumado código de upload de imagem

--- app/models/tourpoint.rb
+++ app/models/tourpoint.rb
@@ -2,9 +2,9 @@ class Tourpoint < ActiveRecord::Base
   has_attached_file :photo,
     :styles => { :large => ["600x600>", :png], :medium => ["300x300>", :png], :thumb => ["100x100>", :png] },
     # O path irá salvar as imagens em sua_app/public/images/user/avatar/1/thumb_originalfilename.jpg
-    :path => ":rails_root/public/images/:class/:attachment/:id/:style_:basename.:extension",
+    :path => ":rails_root/public/images/:class/:attachment/:id/Nervie_:basename_:style.:extension",
     # A url irá ficar: localhost:3000/user/avatar/1/thumb_originalfilename.jpg
-    :url => "/images/:class/:attachment/:id/:style_:basename.:extension"
+    :url => "/images/:class/:attachment/:id/Nervie_:basename_:style.:extension"
 
   # O content_type diz o tipo de extensão que o sistema aceita para validar o upload.
   #validates_attachment_content_type :photo, :content_type => ["image/bmp", "image/x-png", "image/pjpeg", "image/jpg", "image/jpeg", "image/png", "image/gif"]
