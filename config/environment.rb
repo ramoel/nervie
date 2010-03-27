18b0a7142b799a7790479a12d5f50e81bb7b1664
Ramoel Rodrigues de Assis <ramoel@gmail.com>
2010-03-26 21:45:00 -0300

    rails gem version 2.3.6

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = "2.3.6" unless defined? RAILS_GEM_VERSION
 
 ENV['GEM_PATH'] = '/usr/lib/ruby/gems/1.8/'
 # Uncomment below to force Rails into production mode when
