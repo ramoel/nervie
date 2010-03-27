34d7393147dbb806bdf71c177200b5358edd2cd1
Ramoel Rodrigues de Assis <ramoel@gmail.com>
2010-03-27 14:35:41 -0300

    alterado model tourpoint.rb com validação de foto

--- app/models/tourpoint.rb
+++ app/models/tourpoint.rb
@@ -7,8 +7,8 @@ class Tourpoint < ActiveRecord::Base
     :url => "/images/:class/:attachment/:id/Nervie_:basename_:style.:extension"
 
   # O content_type diz o tipo de extensão que o sistema aceita para validar o upload.
-  #validates_attachment_content_type :photo, :content_type => ["image/bmp", "image/x-png", "image/pjpeg", "image/jpg", "image/jpeg", "image/png", "image/gif"]
+  validates_attachment_content_type :photo, :content_type => ["image/bmp", "image/x-png", "image/pjpeg", "image/jpg", "image/jpeg", "image/png", "image/gif"]
 
   # O size diz o tamanho máximo para que seja efetuado o upload na sua aplicação que no caso é de 1 megabyte.
-  #validates_attachment_size :photo, :less_than => 1.megabyte
+  validates_attachment_size :photo, :less_than => 1.megabyte
 end
